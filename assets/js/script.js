9645b8bd0f22fdec7ab0640438378f8b2d924523
Aldrin Burgos <burgos.aldrin@gmail.com>
2024-05-02 20:10:11 -0500

    accept and display user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,22 @@
-const formEl = document.querySelector('#task-form')
+const formEl = document.querySelector('#task-form');
 const taskstoDoEl = document.querySelector('#tasks-to-do');
 
 const createTaskHandler = function(e) {
     e.preventDefault();
 
+    const taskNameInput = document.querySelector("input[name='task-name']").value;
+    const taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    //create list item
     var listItemEl = document.createElement('li');
     listItemEl.className = 'task-item';
-    listItemEl.textContent = "This is a new task.";
-    taskstoDoEl.appendChild(listItemEl);
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement('div');
+    taskInfoEl.className = 'task-info';
+    taskInfoEl.innerHTML ="<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    taskstoDoEl.append(listItemEl);
 }
 
 formEl.addEventListener('submit', createTaskHandler);
